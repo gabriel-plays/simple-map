94516df23eae51b15db7b311b79b6a28e076e1cd
gabriel-plays <gaab4238@gmail.com>
2025-04-02 18:00:06 +0200

    Update map.js

--- map.js
+++ map.js
@@ -7,6 +7,17 @@ const map = new maplibregl.Map({
   attributionControl: false
 });
 
+// Lock viewport to bounds
+const bounds = [
+  [13.072357, -35.061477], // Southwest
+  [35.858002, -21.876792]  // Northeast
+];
+
+map.setMaxBounds(bounds);       // restrict panning
+map.fitBounds(bounds);          // zoom to bounds on load
+map.setMinZoom(map.getZoom()); // prevent zooming out further
+
+
 // Controls
 map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
 
