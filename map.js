b6b920f0da1c10a26a907efe00beb84dd4bb41fd
gabriel-plays <gaab4238@gmail.com>
2025-04-02 13:21:12 +0200

    Update to MapLibre

--- map.js
+++ map.js
@@ -1,45 +1,25 @@
-// Init map
-const map = L.map('map', {
-  zoomControl: false,
+// Init MapLibre map
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
   center: [0, 0],
-  zoom: 2
+  zoom: 2,
+  attributionControl: false
 });
 
-// Basemaps
-const cartoDark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-  subdomains: 'abcd', maxZoom: 19
-});
-cartoDark.addTo(map);
+// Add zoom controls
+map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
 
-const vectorLayer = protomapsL.leafletLayer({
-  url: "https://tiles.gab-plays.work/boundaries.pmtiles",
-  paintRules: [
-    {
-      dataLayer: "boundaries", // this is often 'layer0', can be verified in tile metadata
-      symbolizer: {
-        kind: "fill",
-        color: "#00BFFF",
-        opacity: 0.15,
-        stroke: "#00BFFF",
-        strokeWidth: 1
-      }
-    }
-  ]
-});
+// Geolocation
+function showLocation(position) {
+  const { latitude, longitude } = position.coords;
+  map.setCenter([longitude, latitude]);
+  map.setZoom(14);
 
-vectorLayer.addTo(map);
-
-// Optional: geolocation
-function showLocation(latlng) {
-  map.setView(latlng, 14);
-  L.circleMarker(latlng, {
-    radius: 8,
-    fillColor: '#00BFFF',
-    color: '#fff',
-    weight: 2,
-    opacity: 1,
-    fillOpacity: 0.8
-  }).addTo(map);
+  // Add a marker
+  new maplibregl.Marker({ color: "#00BFFF" })
+    .setLngLat([longitude, latitude])
+    .addTo(map);
 }
 
 function showError() {
@@ -47,21 +27,11 @@ function showError() {
 }
 
 if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-      showLocation([latitude, longitude]);
-    },
-    (error) => {
-      console.warn('Location error:', error.message);
-      showError();
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    }
-  );
+  navigator.geolocation.getCurrentPosition(showLocation, showError, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
 } else {
   showError();
 }
