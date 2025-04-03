37f06d2c8d47f97b65b27cec6169deafb8f81a99
gabriel-plays <gaab4238@gmail.com>
2025-04-03 20:56:24 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,4 @@
 window.onload = () => {
-  const protocol = new pmtiles.Protocol();
-  maplibregl.addProtocol("pmtiles", protocol.tile);
-
   const map = new maplibregl.Map({
     container: "map",
     style: {
@@ -15,10 +12,6 @@ window.onload = () => {
           tileSize: 256,
           attribution:
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
-        },
-        boundaries: {
-          type: "vector",
-          url: "pmtiles://https://gab-plays.work/tiles/boundaries.pmtiles"
         }
       },
       layers: [
@@ -26,30 +19,10 @@ window.onload = () => {
           id: "carto",
           type: "raster",
           source: "carto"
-        },
-        {
-          id: "boundaries-fill",
-          type: "fill",
-          source: "boundaries",
-          "source-layer": "boundaries",
-          paint: {
-            "fill-color": "#ffcc00",
-            "fill-opacity": 0.5
-          }
-        },
-        {
-          id: "boundaries-outline",
-          type: "line",
-          source: "boundaries",
-          "source-layer": "boundaries",
-          paint: {
-            "line-color": "#ffffff",
-            "line-width": 1
-          }
         }
       ]
     },
-    center: [18.4241, -33.9249], // Cape Town
-    zoom: 10
+    center: [0, 0],
+    zoom: 2
   });
 };
